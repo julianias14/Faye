f7284ff9ad72d900565e8adc3131d847288d771b
julianias14 <julianias14@gmail.com>
2026-04-04 01:23:47 -0400

    added flower to be able to appear in a corner; hardcoded for april 4th 2026

--- content.js
+++ content.js
@@ -1,15 +1,26 @@
 console.log("Faye loaded!");
 
-function findCells() {
-    const cells = document.querySelectorAll("td[data-date]");
+const flowerURL = chrome.runtime.getURL("flower.png");
 
-    cells.forEach(cell => {
-    const date = cell.getAttribute("data-date");
-    
-    if (date === "20260404") {
-      console.log("Match found! Placing flower on", date);
+function findCells() {
+  const cells = document.querySelectorAll("div[data-datekey]");
+
+  cells.forEach(cell => {
+    const key = cell.getAttribute("data-datekey");
+
+    if (key === "28804" && !cell.querySelector(".period-flower")) {
+      const img = document.createElement("img");
+      img.src = flowerURL;
+      img.className = "period-flower";
+      img.style.height = "25px";
+      img.style.width = "25px";
+      img.style.top = "5px";
+      img.style.right = "5px";
+      img.style.position = "absolute";
+      cell.style.position = "relative";
+      cell.appendChild(img);
     }
-    });
+  });
 }
 
 const observer = new MutationObserver(() => {

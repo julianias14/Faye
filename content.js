f424000dc8df51a30c71420aedbffdc5897e48a4
julianias14 <julianias14@gmail.com>
2026-04-05 22:59:03 -0400

    add code to anaylze json file and add flowers to calendar based on data

--- content.js
+++ content.js
@@ -2,23 +2,49 @@ console.log("Faye loaded!");
 
 const flowerURL = chrome.runtime.getURL("flower.png");
 
+function dateToKey(dateStr) {
+  const date = new Date(dateStr);
+  const epoch = new Date("2000-01-01");
+  const diffDays = Math.round((date - epoch) / (1000 * 60 * 60 * 24));
+  return 19215+diffDays;
+}
+
+fetch(chrome.runtime.getURL("measurements.json"))
+  .then(response => response.json())
+  .then(data => {
+    const markedKeys = new Set();
+    data.forEach(entry => {
+      if (entry.type === "period" || entry.type === "spotting") {
+        const key = dateToKey(entry.date);
+        markedKeys.add(key);
+      }
+    });
+    console.log("Marked keys:", [...markedKeys].sort((a,b) => a-b));
+
+function addFlower(cell) {
+    if (cell.querySelector(".period-flower"))
+        return;
+    const img = document.createElement("img");
+    img.src = flowerURL;
+    img.className = "period-flower";
+    img.style.height = "25px";
+    img.style.width = "25px";
+    img.style.top = "5px";
+    img.style.right = "5px";
+    img.style.position = "absolute";
+    cell.style.position = "relative";
+    cell.appendChild(img);
+}
+
 function findCells() {
   const cells = document.querySelectorAll("div[data-datekey]");
 
   cells.forEach(cell => {
-    const key = cell.getAttribute("data-datekey");
+    const key = parseInt(cell.getAttribute("data-datekey"));
+    console.log("Cell key:", key);
 
-    if (key === "28804" && !cell.querySelector(".period-flower")) {
-      const img = document.createElement("img");
-      img.src = flowerURL;
-      img.className = "period-flower";
-      img.style.height = "25px";
-      img.style.width = "25px";
-      img.style.top = "5px";
-      img.style.right = "5px";
-      img.style.position = "absolute";
-      cell.style.position = "relative";
-      cell.appendChild(img);
+    if (markedKeys.has(key)) {
+      addFlower(cell)
     }
   });
 }
@@ -33,3 +59,4 @@ observer.observe(document.body, {
 });
 
 findCells();
+});
